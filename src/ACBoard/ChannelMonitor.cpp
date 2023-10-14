a7d73b1db4c90cf90eb4514e5e55406588c0807c
C.T. Chen <ctychen@berkeley.edu>
2023-10-14 16:18:05 -0700

    Fixed red LED

--- src/ACBoard/ChannelMonitor.cpp
+++ src/ACBoard/ChannelMonitor.cpp
@@ -108,16 +108,16 @@ uint32_t readChannels() {
         // handle LEDs
 
         int mapping[8] = {6, 3, 7, 2, 4, 5, 0, 1};
-        if (cont > CONT_THRESHOLD) {
-            //set green
-            setLED(mapping[i], Adafruit_NeoPixel::Color(0, 255, 0), currentColor[i]);
-            currentColor[i] = Adafruit_NeoPixel::Color(0, 255, 0);
-        }
-        else if (curr > RUNNING_THRESH) {
+        if (curr > RUNNING_THRESH) {
             //set red
             setLED(mapping[i], Adafruit_NeoPixel::Color(255, 0, 0), currentColor[i]);
             currentColor[i] = Adafruit_NeoPixel::Color(255, 0, 0);
 
+        }
+        else if (cont > CONT_THRESHOLD) {
+            //set green
+            setLED(mapping[i], Adafruit_NeoPixel::Color(0, 255, 0), currentColor[i]);
+            currentColor[i] = Adafruit_NeoPixel::Color(0, 255, 0);
         }
         else {
             //set to white

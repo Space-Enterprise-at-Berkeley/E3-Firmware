0798b4ca7fb7ec8803f9e1abf022b2f6d7183ec2
Walter Cheng <chengwaltj@gmail.com>
2025-10-01 20:29:19 -0700

    updated eth-tx

--- src/tests/ethernet-tx-test/main.cpp
+++ src/tests/ethernet-tx-test/main.cpp
@@ -3,49 +3,45 @@
 #include <EthernetUdp.h>
 #include <Ethernet.h>
 
+#define ETH_CS   10
+#define ETH_SCK  12
+#define ETH_MISO 13
+#define ETH_MOSI 11
+#define ETH_INT   9   // if you wired it and want to use it
+
 int count;
 EthernetUDP Udp;
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x99};
-IPAddress groundStation1(10, 0, 0, 169);
+IPAddress groundStation1(10, 0, 0, 177);
 IPAddress ip(10, 0, 0, 99);
 int port = 42069;
 
 void setup()
 {
   Serial.begin(921600);
-  Ethernet.init(10);
-  Ethernet.begin((uint8_t *)mac, ip);
-  Udp.begin(port);
 
-  Udp.beginPacket(groundStation1, port);
+  // Map SPI pins to your wiring
+  SPI.begin(ETH_SCK, ETH_MISO, ETH_MOSI, ETH_CS);
+
+  pinMode(ETH_CS, OUTPUT);
+  digitalWrite(ETH_CS, HIGH);
+
+  Ethernet.init(ETH_CS);
+  if (Ethernet.begin(mac) == 0) {
+    Ethernet.begin(mac, ip);
+  }
+
+  Udp.begin(42069);
+
+  delay(200);
+  Serial.println(Ethernet.localIP());
 }
 
 void loop()
 {
-  // Serial.println("before begin");
-  // Serial.flush();
   while(1) {
-    Udp.resetSendOffset();
-    // Serial.println("before write");
-    // Serial.flush();
-    char tosend[] = "itimelckdatadatadatadata";
-    // uint32_t beforeWrite = micros();
-    Udp.write((unsigned char *) tosend, 24);
-    // uint32_t beforeEnd = micros();
-    // Serial.println("before end");
-    // Serial.flush();
+    Udp.beginPacket(groundStation1, port);
+    Udp.write("fuck you");
     Udp.endPacket();
-    // uint32_t end = micros();
-
-    // Serial.print("write: ");
-    // Serial.print(beforeEnd - beforeWrite);
-    // Serial.print(" end: ");
-    // Serial.println(end - beforeEnd);
   }
-
-  // Serial.println(count);
-  // Serial.flush();
-  // count += 1;
-
-  // delay(1000);
 }

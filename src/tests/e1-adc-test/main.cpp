f59a4384b85354594b8361abce9aee1cb7227c97
Thanh Luu <thanh.luu@berkeley.edu>
2025-10-17 18:15:25 -0700

    poppet testing configs

--- src/tests/e1-adc-test/main.cpp
+++ src/tests/e1-adc-test/main.cpp
@@ -9,18 +9,20 @@ SPIClass *spi2;
 void setup(){
     Serial.begin(921600);
     spi2 = new SPIClass(HSPI);
-    spi2->begin(41, 42, 40, 39);
-    adc.init(spi2, 39, 38);
+    // spi2->begin(41, 42, 40, 39);
+    // adc.init(spi2, 39, 38);
+    spi2->begin(19, 18, 17, 20);
+    adc.init(spi2, 20, 21);
     adc.setAllInputsSeparate();
     adc.enableOTFMode();
-    pinMode(18, OUTPUT);
-    pinMode(19, OUTPUT);
-    pinMode(20, OUTPUT);
-    pinMode(21, OUTPUT);
-    pinMode(26, OUTPUT);
-    pinMode(33, OUTPUT);
-    pinMode(34, OUTPUT);
-    pinMode(35, OUTPUT);
+    // pinMode(18, OUTPUT);
+    // pinMode(19, OUTPUT);
+    // pinMode(20, OUTPUT);
+    // pinMode(21, OUTPUT);
+    // pinMode(26, OUTPUT);
+    // pinMode(33, OUTPUT);
+    // pinMode(34, OUTPUT);
+    // pinMode(35, OUTPUT);
 
     while(1) {
         Serial.print("adc0: ");
@@ -39,26 +41,26 @@ void setup(){
         Serial.print(adc.readData(6));
         Serial.print("   adc7: ");
         Serial.println(adc.readData(7));
-        digitalWrite(21, LOW);
-        digitalWrite(35, LOW);
-        digitalWrite(18, HIGH);
-        digitalWrite(26, HIGH);
-        delay(100);
-        digitalWrite(18, LOW);
-        digitalWrite(26, LOW);
-        digitalWrite(19, HIGH);
-        digitalWrite(33, HIGH);
-        delay(100);
-        digitalWrite(19, LOW);
-        digitalWrite(33, LOW);
-        digitalWrite(20, HIGH);
-        digitalWrite(34, HIGH);
-        delay(100);
-        digitalWrite(20, LOW);
-        digitalWrite(34, LOW);
-        digitalWrite(21, HIGH);
-        digitalWrite(35, HIGH);
-        delay(200);
+        // digitalWrite(21, LOW);
+        // digitalWrite(35, LOW);
+        // digitalWrite(18, HIGH);
+        // digitalWrite(26, HIGH);
+        // delay(100);
+        // digitalWrite(18, LOW);
+        // digitalWrite(26, LOW);
+        // digitalWrite(19, HIGH);
+        // digitalWrite(33, HIGH);
+        // delay(100);
+        // digitalWrite(19, LOW);
+        // digitalWrite(33, LOW);
+        // digitalWrite(20, HIGH);
+        // digitalWrite(34, HIGH);
+        // delay(100);
+        // digitalWrite(20, LOW);
+        // digitalWrite(34, LOW);
+        // digitalWrite(21, HIGH);
+        // digitalWrite(35, HIGH);
+        // delay(200);
     }
 
     //on the fly testing
--- src/tests/hello-world/main.cpp
+++ src/tests/hello-world/main.cpp
@@ -1,16 +1,56 @@
 
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+//LED Setup
+constexpr uint8_t  LED_PIN_1  = 4;      // IO4
+constexpr uint8_t  LED_PIN_2  = 5;      // IO5
+constexpr uint16_t LED_COUNT  = 8;      // set to your number of pixels
+constexpr uint8_t  BRIGHTNESS = 40;
+constexpr uint8_t  PHASE_SHIFT = 15;   // 0..255 (128 ≈ 180°)  <-- added
+
+
+Adafruit_NeoPixel strip1(LED_COUNT, LED_PIN_1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2(LED_COUNT, LED_PIN_2, NEO_GRB + NEO_KHZ800);
+
+
+uint32_t Wheel(byte pos) {
+  if (pos < 85) {
+    return strip1.Color(pos * 3, 255 - pos * 3, 0);
+  } else if (pos < 170) {
+    pos -= 85;
+    return strip1.Color(255 - pos * 3, 0, pos * 3);
+  } else {
+    pos -= 170;
+    return strip1.Color(0, pos * 3, 255 - pos * 3);
+  }
+}
+
+
+
 
 void setup() {
   Serial.begin(921600);
-  pinMode(37, OUTPUT);
-  
+  strip1.begin();
+  strip2.begin();
+  strip1.setBrightness(BRIGHTNESS);
+  strip2.setBrightness(BRIGHTNESS);
+  strip1.show(); 
+  strip2.show();  
 }
 
 void loop() {
-    Serial.println("hello World");
-    delay(500);
+    for (int j = 0; j < 256; j++) { // 0-255 colors
+    for (int i = 0; i < LED_COUNT; i++) {
+      // Spread rainbow across pixels with an offset
+      strip1.setPixelColor(i, Wheel((i * 256 / LED_COUNT + j) & 255));
+      strip2.setPixelColor(i, Wheel((i * 256 / LED_COUNT + j + PHASE_SHIFT) & 255));  // <-- changed
+    }
+    strip1.show();
+    strip2.show();
+    delay(5); // speed of rainbow
+  
+}
+}
     
-    digitalWrite(37, HIGH);
 
-} // unused
